7294915520607e55e69e705c4c7977bf3c949cf1
Yauheni Malashchytski <yauheni_malashchytski@epam.com>
2017-05-03 17:02:39 +0300

    bootstrap added for jsp

--- Controller/src/main/java/com/hustlestar/airbnb/WebAppInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.hustlestar.airbnb;
-
-/**
- * Created by Yauheni_Malashchytsk on 4/26/2017.
- */
-public class Some {
-}
--- Controller/src/main/java/com/hustlestar/airbnb/WebConfig.java
+++
@@ -1,7 +0,0 @@
-package com.hustlestar.airbnb;
-
-/**
- * Created by Yauheni_Malashchytsk on 4/29/2017.
- */
-public class WebConfig {
-}
